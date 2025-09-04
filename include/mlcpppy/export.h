3137a40044ba63db36fa333851e8951c16332818
Pedro Bianchini de Quadros <pedrobiqua@outlook.com>
2025-09-04 16:33:36 -0300

    shared lib

---
+++ include/mlcpppy/export.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef _WIN32
+  #ifdef MLCPPPY_EXPORTS
+    #define MLCPPPY_API __declspec(dllexport)
+  #else
+    #define MLCPPPY_API __declspec(dllimport)
+  #endif
+#else
+  #define MLCPPPY_API
+#endif
