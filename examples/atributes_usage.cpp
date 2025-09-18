af02b2591dbf5e464630fc5b3c7d5ab71cd85e74
Pedro Bianchini de Quadros <pedrobiqua@outlook.com>
2025-09-18 10:58:55 -0300

    feat(Instances): add struct for instances

---
+++ examples/atributes_usage.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "mlcpppy/instances/instances.h"
+
+int main(int argc, char const *argv[])
+{
+    // Exemplo de uso da classe atribute, instances e instance
+    Instance a(Attribute(1), Attribute(2.5), Attribute("Pedro"));
+    Instance b(Attribute(1), Attribute(2.5), Attribute("Pedro"));
+    Instance c(Attribute(1), Attribute(2.5), Attribute("Pedro"));
+    Instances matrix({a,b,c});
+    std::cout << matrix << std::endl;
+    return 0;
+}
